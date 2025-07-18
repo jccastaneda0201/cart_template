0f819c02a0105cb94baff5dc4e8e335fe00719e8
Juan Castaneda <jccastanedacamargo@outlook.com>
2025-07-18 17:09:49 +0200

    create action and reducer js files

--- src/context.jsx
+++ src/context.jsx
@@ -1,10 +1,19 @@
 import { useContext, useReducer, useEffect, createContext } from 'react';
+import reducer from './reducer';
+import cartItems from './cart_items';
+import { CLEAR_CART, REMOVE, INCREASE, DECREASE, LOADING, DISPLAY_ITEMS } from './actions';
 
 const appContext = createContext();
 
+const initialState = {
+  loading: false,
+  cartItems: [],
+};
+
 export const AppProvider = ({ children }) => {
-  const greeting = 'hello';
-  return <appContext.Provider value={{ greeting }}>{children}</appContext.Provider>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return <appContext.Provider value={{ ...state }}>{children}</appContext.Provider>;
 };
 
 export const useGlobalContext = () => {
